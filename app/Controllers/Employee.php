4e03011c8cb1c4894278a1172e7f4a61a513ea92
resha007 <reshans777@gmail.com>
2021-12-26 11:07:07 +0530

    emp half

---
+++ app/Controllers/Employee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Employee extends BaseController
+{
+    public function index()
+    {
+        return view('employee');
+    }
+}
--- app/Views/employee.php
+++ app/Views/employee.php
@@ -7,6 +7,7 @@
 
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/datepicker3.css" rel="stylesheet">
+<link href="css/bootstrap-table.css" rel="stylesheet">
 <link href="css/styles.css" rel="stylesheet">
 
 <!--Icons-->
@@ -29,7 +30,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#"><span>Lumino</span>Admin</a>
+				<a class="navbar-brand" href="#"><span>MICRO</span>fin</a>
 				<ul class="user-menu">
 					<li class="dropdown pull-right">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> User <span class="caret"></span></a>
@@ -97,131 +98,107 @@
 		
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header">Forms</h1>
+				<h1 class="page-header">Employee</h1>
 			</div>
 		</div><!--/.row-->
-				
+		
 		
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="panel panel-default">
-					<div class="panel-heading">Form Elements</div>
+					<div class="panel-heading">Employee Details</div>
+						<div class="panel-body">
+							<div class="col-md-12">
+								<form role="form">
+								
+									<div class="form-group col-md-6">
+										<label>First Name</label>
+										<input class="form-control" name="first_name" id="first_name">
+									</div>
+																	
+									<div class="form-group col-md-6">
+										<label>Last Name</label>
+										<input class="form-control" name="last_name" id="last_name">
+									</div>
+									<div class="form-group col-md-6">
+										<label>Address</label>
+										<input class="form-control" name="address" id="address">
+									</div>					
+									<div class="form-group col-md-6">
+										<label>City</label>
+										<input class="form-control" name="city" id="city">
+									</div>
+									<div class="form-group col-md-4">
+										<label>DoB</label>
+										<input type="date" class="form-control" name="dob" id="dob">
+									</div>
+									<div class="form-group col-md-4">
+										<label>NIC</label>
+										<input class="form-control" name="nic" id="nic">
+									</div>
+									<div class="form-group col-md-4">
+										<label>Gender</label>
+										<select class="form-control" name="gender" id="gender">
+											<option value="1">Male</option>
+											<option value="2">Female</option>
+										</select>
+									</div>
+									<div class="form-group col-md-4">
+										<label>Contact Number</label>
+										<input class="form-control" name="contact_no" id="contact_no">
+									</div>
+									<div class="form-group col-md-4">
+										<label>Email</label>
+										<input type="email" class="form-control" name="email" id="email">
+									</div>
+									<div class="form-group col-md-4">
+										<label>Username</label>
+										<input class="form-control" name="username" id="username">
+									</div>
+									<div class="form-group col-md-4">
+										<label>Type</label>
+										<select class="form-control" name="type" id="type">
+											<option value="1">Employee</option>
+											<option value="2">Rider</option>
+										</select>
+									</div>
+
+
+									<div class="form-group col-md-12">
+										<button type="submit" class="btn btn-primary">Submit Button</button>
+										<button type="reset" class="btn btn-default">Reset Button</button>
+									</div>
+							</div>
+							
+					</div>
+				</div>
+			</div><!-- /.col-->
+		</div><!-- /.row -->
+
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Current Employees</div>
 					<div class="panel-body">
-						<div class="col-md-6">
-							<form role="form">
-							
-								<div class="form-group">
-									<label>Text Input</label>
-									<input class="form-control" placeholder="Placeholder">
-								</div>
-																
-								<div class="form-group">
-									<label>Password</label>
-									<input type="password" class="form-control">
-								</div>
+						<table data-toggle="table" data-url="tables/data1.json"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
+								<thead>
+								<tr>
+									<th data-field="state" data-checkbox="true" >Item ID</th>
+									<th data-field="id" data-sortable="true">Item ID</th>
+									<th data-field="name"  data-sortable="true">Item Name</th>
+									<th data-field="price" data-sortable="true">Item Price</th>
+								</tr>
+								</thead>
+								<tr>
+									<td>11</td>
+									<td>1</td>
+									<td>1</td>
+									<td>1</td>
+								</tr>
 								
-								<div class="form-group checkbox">
-								  <label>
-								    <input type="checkbox">Remember me</label>
-								</div>
-																
-								<div class="form-group">
-									<label>File input</label>
-									<input type="file">
-									 <p class="help-block">Example block-level help text here.</p>
-								</div>
 								
-								<div class="form-group">
-									<label>Text area</label>
-									<textarea class="form-control" rows="3"></textarea>
-								</div>
-								
-								<label>Validation</label>
-								<div class="form-group has-success">
-									<input class="form-control" placeholder="Success">
-								</div>
-								<div class="form-group has-warning">
-									<input class="form-control" placeholder="Warning">
-								</div>
-								<div class="form-group has-error">
-									<input class="form-control" placeholder="Error">
-								</div>
-								
-							</div>
-							<div class="col-md-6">
-							
-								<div class="form-group">
-									<label>Checkboxes</label>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" value="">Checkbox 1
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" value="">Checkbox 2
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" value="">Checkbox 3
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" value="">Checkbox 4
-										</label>
-									</div>
-								</div>
-								
-								<div class="form-group">
-									<label>Radio Buttons</label>
-									<div class="radio">
-										<label>
-											<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Radio Button 1
-										</label>
-									</div>
-									<div class="radio">
-										<label>
-											<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Radio Button 2
-										</label>
-									</div>
-									<div class="radio">
-										<label>
-											<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Radio Button 3
-										</label>
-									</div>
-									<div class="radio">
-										<label>
-											<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Radio Button 4
-										</label>
-									</div>
-								</div>
-								
-								<div class="form-group">
-									<label>Selects</label>
-									<select class="form-control">
-										<option>Option 1</option>
-										<option>Option 2</option>
-										<option>Option 3</option>
-										<option>Option 4</option>
-									</select>
-								</div>
-								
-								<div class="form-group">
-									<label>Multiple Selects</label>
-									<select multiple class="form-control">
-										<option>Option 1</option>
-										<option>Option 2</option>
-										<option>Option 3</option>
-										<option>Option 4</option>
-									</select>
-								</div>
-								
-								<button type="submit" class="btn btn-primary">Submit Button</button>
-								<button type="reset" class="btn btn-default">Reset Button</button>
-							</div>
-						</form>
+							</table>
 					</div>
 				</div>
 			</div><!-- /.col-->
@@ -236,6 +213,7 @@
 	<script src="js/easypiechart.js"></script>
 	<script src="js/easypiechart-data.js"></script>
 	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/bootstrap-table.js"></script>
 	<script>
 		!function ($) {
 			$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
