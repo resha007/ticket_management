d728628b23b22d844c78822401262c6d1db1fa15
resha007 <reshans777@gmail.com>
2021-12-27 13:08:22 +0530

    db with loan,payment,repayment schedule

--- app/Database/microfin.sql
+++ app/Database/microfin.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 26, 2021 at 05:56 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 27, 2021 at 07:37 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.4.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -27,8 +28,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(200) NOT NULL,
   `last_name` varchar(200) NOT NULL,
   `nic` varchar(12) NOT NULL,
@@ -39,7 +41,8 @@ CREATE TABLE `customer` (
   `gender` tinyint(1) NOT NULL,
   `dob` date NOT NULL,
   `status` tinyint(1) NOT NULL,
-  `line_id` int(11) NOT NULL
+  `line_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -48,8 +51,9 @@ CREATE TABLE `customer` (
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(200) NOT NULL,
   `last_name` varchar(200) NOT NULL,
   `gender` tinyint(1) NOT NULL,
@@ -61,7 +65,8 @@ CREATE TABLE `employee` (
   `city` varchar(200) NOT NULL,
   `type` tinyint(1) NOT NULL,
   `contact_no` varchar(10) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -70,14 +75,78 @@ CREATE TABLE `employee` (
 -- Table structure for table `line`
 --
 
-CREATE TABLE `line` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `line`;
+CREATE TABLE IF NOT EXISTS `line` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(100) NOT NULL,
   `name` varchar(200) NOT NULL,
   `rider_id` int(11) NOT NULL,
   `opt_rider_id` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+CREATE TABLE IF NOT EXISTS `loan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `loan_amount` int(11) NOT NULL,
+  `loan_period` int(11) NOT NULL,
+  `loan_interest` double NOT NULL,
+  `reason` text NOT NULL,
+  `guarantor_1` int(11) NOT NULL,
+  `guarantor_2` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `approved_by` int(11) NOT NULL,
+  `apprioved_date` datetime NOT NULL,
+  `effective_date` datetime NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `repayment_id` int(11) NOT NULL,
+  `loan_id` int(11) NOT NULL,
+  `paid_date` datetime NOT NULL,
+  `paid_amount` float NOT NULL,
+  `rider_id` int(11) NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `repayment_schedule`
+--
+
+DROP TABLE IF EXISTS `repayment_schedule`;
+CREATE TABLE IF NOT EXISTS `repayment_schedule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `loan_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `amount` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
