523f1f78337ebe23b33b155eb94a27fde71a8e24
resha007 <reshans777@gmail.com>
2021-12-30 08:15:21 +0530

    base url updated

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/microfin/';
+    public $baseURL = 'http://localhost/microfin/public';
 
     /**
      * --------------------------------------------------------------------------
