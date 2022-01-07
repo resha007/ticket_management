b2e745e8d2a5a1df1e48798f7554ccb782821051
resha007 <reshans777@gmail.com>
2022-01-07 10:56:36 +0530

    customer completed

---
+++ app/Controllers/customer.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\CustomerModel;
+use CodeIgniter\Controller;
+
+class Customer extends Controller
+{
+
+    public function index()
+    {
+
+		return view('customer');
+    }
+
+    function get(){ 
+        $CustomerModel = new CustomerModel();
+
+		//$data = $CustomerModel->where("status='1' OR status='2'")->orderBy('id', 'ASC')->paginate(10);
+        $data = $CustomerModel->get_data();
+
+        //set numbers to names
+        for($i=0;$i<sizeof($data);$i++){
+            if($data[$i]["gender"] == 1){
+                $data[$i]["gender"] = "Male";
+            }else{
+                $data[$i]["gender"] = "Female";
+            }
+
+            if($data[$i]["status"] == 1){
+                $data[$i]["status"] = "Active";
+            }else if($data[$i]["status"] == 2){
+                $data[$i]["status"] = "Inactive";
+            }
+        }
+        
+
+		echo json_encode($data);
+	}
+
+    
+	function save(){
+        helper(['form', 'url']);
+        
+        $model = new CustomerModel();
+        
+        $data = [
+            'first_name'	=>	$this->request->getPost('first_name'),
+            'last_name'	    =>	$this->request->getPost('last_name'),
+            'address'        =>	$this->request->getPost('address'),
+            'city'	=>	$this->request->getVar('city'),
+            'dob'	    =>	$this->request->getVar('dob'),
+            'nic'        =>	$this->request->getVar('nic'),
+            'gender'	=>	$this->request->getVar('gender'),
+            'contact_no'	    =>	$this->request->getVar('contact_no'),
+            'email'        =>	$this->request->getVar('email'),
+            'line_id'        =>	$this->request->getVar('line'),
+            'status'        =>	$this->request->getVar('status')
+        ];
+
+        $save_data = $model->insert_data($data);
+
+        if($save_data != false){
+            //$data = $model->where('id', $save_data)->first();
+            echo json_encode(array("status" => true , 'data' => $data));
+        }else{
+            echo json_encode(array("status" => false , 'data' => $data));
+        }
+    }
+
+    function update(){
+        helper(['form', 'url']);
+        
+        $model = new CustomerModel();
+
+        $id = $this->request->getPost('id');
+        
+        $data = [
+            'first_name'	=>	$this->request->getPost('first_name'),
+            'last_name'	    =>	$this->request->getPost('last_name'),
+            'address'        =>	$this->request->getPost('address'),
+            'city'	=>	$this->request->getVar('city'),
+            'dob'	    =>	$this->request->getVar('dob'),
+            'nic'        =>	$this->request->getVar('nic'),
+            'gender'	=>	$this->request->getVar('gender'),
+            'contact_no'	    =>	$this->request->getVar('contact_no'),
+            'email'        =>	$this->request->getVar('email'),
+            'line_id'        =>	$this->request->getVar('line'),
+            'status'        =>	$this->request->getVar('status')
+        ];
+
+        $result = $model->update_data($id, $data);
+
+        if($result != false){
+            //$data = $model->where('id', $save_data)->first();
+            echo json_encode(array("status" => true , 'data' => $data));
+        }else{
+            echo json_encode(array("status" => false , 'data' => $data));
+        }
+        
+    }
+
+    function delete(){
+        helper(['form', 'url']);
+        
+        $model = new CustomerModel();
+
+        $id = $this->request->getPost('id');
+        
+        $data = [
+            'status'        =>	3
+        ];
+
+        $result = $model->delete_data($id, $data);
+
+        if($result != false){
+            //$data = $model->where('id', $save_data)->first();
+            echo json_encode(array("status" => true , 'data' => $data));
+        }else{
+            echo json_encode(array("status" => false , 'data' => $data));
+        }
+        
+    }
+
+    function by_id(){
+        helper(['form', 'url']);
+        
+        $model = new CustomerModel();
+
+        $id = $this->request->getPost('id');
+
+        $result = $model->get_data_by_id($id);
+
+        if($result != false){
+            //$data = $model->where('id', $save_data)->first();
+            echo json_encode(array("status" => true , 'data' => $result));
+        }else{
+            echo json_encode(array("status" => false , 'data' => $result));
+        }
+        
+    }
+
+	
+}
---
+++ app/Models/CustomerModel.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use CodeIgniter\Database\ConnectionInterface;
+
+class CustomerModel extends Model
+{
+    protected $table = 'customer';
+
+    protected $primaryKey = 'id';
+    
+    protected $allowedFields = ['id','first_name', 'last_name','address','city','dob','nic','gender','contact_no','email','line_id','password','status'];
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get_data($id = false) {//$id=1;
+        if($id === false) {
+            // $this->join('line', 'line.id = customer.line_id', 'LEFT');
+            // $this->select('customer.*');
+            // $this->select("CONCAT(line.code, ' - ', line.name) as line");
+            // //$this->where('status', 1);
+            // return $this->findAll();
+            return $this->join('line', 'line.id = customer.line_id', 'LEFT')->select('customer.*')->select("CONCAT(line.code, ' - ', line.name) as line")->where('customer.status', 1)->orWhere('customer.status', 2)->findAll();
+            //return $this->where('status', 1)->orWhere('status', 2)->findAll();
+        } else {
+            return $this->where('status', 1)->findAll();
+        }
+    }
+
+    public function insert_data($data) {
+        if($this->insert($data)){
+                return true;
+            }
+            else{
+                return false;
+            }
+    }
+
+    public function update_data($id,$data) {
+        if($this->update($id,$data)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function delete_data($id,$data) {
+        if($this->update($id,$data)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public function get_data_by_id($id) {
+        return $this->where('id', $id)->first();
+
+    }
+
+}
+
+?>
